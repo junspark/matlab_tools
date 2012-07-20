44b811ce6daea3b284014b75a7f6a3691006d9b5
Jun-Sang Park <jp118@cornell.edu>
2012-07-20 15:39:39 -0400

    x-ray conversion

---
+++ xray-calculations/Angstrom2keV.m
@@ -0,0 +1,19 @@
+function keV = Angstrom2keV(Angstrom)
+% Angstrom2keV - converts x-ray wavelength in angstrom to energy in keV
+%
+%   USAGE:
+%
+%   keV = Angstrom2keV(Angstrom)
+%
+%   INPUT:
+%
+%   Angstrom
+%       x-ray wavelength in angstroms
+%
+%   OUTPUT:
+%
+%   keV
+%       x-ray energy in keV
+
+hc  = 12.39842; % PER X-RAY BOOKLET 2009
+keV = hc/Angstrom;
---
+++ xray-calculations/keV2Angstrom.m
@@ -0,0 +1,19 @@
+function Angstrom = keV2Angstrom(keV)
+% keV2Angstrom - converts x-ray energy in keV to wavelength in angstroms
+%
+%   USAGE:
+%
+%   Angstrom = keV2Angstrom(keV)
+%
+%   INPUT:
+%
+%   keV
+%       x-ray energy in keV
+%
+%   OUTPUT:
+%
+%   Angstrom
+%       x-ray wavelength in anstroms
+
+hc  = 12.39842; % PER X-RAY BOOKLET 2009
+Angstrom    = hc/keV;
