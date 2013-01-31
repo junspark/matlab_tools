92539cd03a4fcf7b52f2fba54ccc2933d888d9fd
Jun-Sang Park <jp118@cornell.edu>
2013-01-31 12:11:34 -0500

    updates to NreadGE
    added help information

--- image-processing/NreadGE.m
+++ image-processing/NreadGE.m
@@ -1,4 +1,24 @@
 function img = NreadGE(filename, frameno)
+% NreadGE - read GE file from fastsweep image stack
+%
+%   INPUT:
+%
+%   filename
+%       name of the GE image stack file name
+%
+%   frameno
+%       frame to load
+%
+%   OUTPUT:
+%
+%   img
+%       image data in a [2048 x 2048] matrix
+%
+%   NOTE:
+%   1. if the image size changes, this file needs to be updated.
+%   2. if the buffer contains useful information, this file needs to read
+%   it in.
+
 buffer  = 8192;
 
 fp      = fopen(filename,'r','n');
