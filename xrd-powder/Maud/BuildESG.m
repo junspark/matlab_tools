5f44bb626afdba345b5940799294eec796ef193c
Jun-Sang Park <jp118@cornell.edu>
2013-01-31 15:38:57 -0500

    maud input / output functionality

---
+++ xrd-powder/Maud/BuildESG.m
@@ -0,0 +1,76 @@
+function BuildESG(instr, cakeParms, polImgCor)
+% BuildESG - builds ESB file for MAUD input
+%
+%   USAGE:
+%
+%   BuildESG(instr, cakeParms, polImgCor)
+%
+%   INPUT:
+%
+%   instr
+%           is the structure array that defines the x-ray instrument
+%
+%   cakeParms
+%           is the structure array that defines the caking parameters
+% 
+%   polImgCor
+%           is the caked data generated using instr and cakeParms
+%
+%   OUTPUT:  none
+%
+r=polImgCor.radius;
+I=polImgCor.intensity;
+fnameESG=[instr.fileName '.esg'];
+disp(['writing ', fnameESG])
+fid = fopen(fnameESG,'w');
+A={'_pd_block_id noTitle|#0';
+    '';
+    '_diffrn_detector Image Plate';
+    '_diffrn_detector_type Image Plate';
+    '_pd_meas_step_count_time ?';
+    '_diffrn_measurement_method ?';
+    '_diffrn_measurement_distance_unit mm';
+    ['_pd_instr_dist_spec/detc ' num2str(instr.distance)];
+    ['_diffrn_radiation_wavelength ' num2str(instr.wavelength)];
+    '_diffrn_source_target ?';
+    '_diffrn_source_power ?';
+    '_diffrn_source_current ?';
+    ['_pd_meas_angle_omega ' num2str(instr.omega)];
+    ['_pd_meas_angle_chi ' num2str(instr.chi)];
+    '_pd_meas_angle_phi 0.0';
+    '_riet_par_spec_displac_x 0';
+    '_riet_par_spec_displac_y 0';
+    '_riet_par_spec_displac_z 0';
+    '_riet_meas_datafile_calibrated false';
+    '_pd_meas_angle_eta 0.0';
+    '';
+    'loop_';
+    '_pd_proc_2theta_corrected';
+    '_pd_calc_intensity_total';};
+for yyy=1:size(A,1);
+    fprintf(fid, '%s', A{yyy});
+    fprintf(fid, '\n');
+end
+
+for xxx=1:size(r,1)
+    for yyy=1:size(r,2)
+        fprintf(fid, '%d %d', [r(xxx,size(r,2)-yyy+1) I(xxx,size(r,2)-yyy+1)]);
+        fprintf(fid, '\n');
+    end
+    
+    if xxx<size(r,1)
+        A={'';
+            ['_pd_block_id noTitle|#' num2str(xxx)];
+            '';
+            ['_pd_meas_angle_eta ' num2str(cakeParms.azim(xxx+1))];
+            '';
+            'loop_';
+            '_pd_proc_2theta_corrected';
+            '_pd_calc_intensity_total';};
+        for yyy=1:size(A,1);
+            fprintf(fid, '%s', A{yyy});
+            fprintf(fid, '\n');
+        end
+    end
+end
+fclose(fid);
---
+++ xrd-powder/Maud/ReadAPF.m
@@ -0,0 +1,64 @@
+function apf_data = ReadAPF(fname)
+% ReadAPF - reads APF file from MAUD
+%
+%   USAGE:
+%
+%   ReadAPF(fname)
+%
+%   INPUT:
+%
+%   fname
+%           name of the APF file
+%
+%   OUTPUT:
+%
+%   apf_data
+%			is the pole figure data extracted from 
+%			x-ray diffraction spectrum after MAUD analysis	
+%
+
+% clear all
+% close all
+% clc
+% fname   = '/home/jspark/Documents/work/prjTitanium/DataReduction/MA-Ti/texture/Ti_hcp_dv0.apf';
+fid     = fopen(fname, 'r');
+
+linedata	= fgetl(fid);
+idx         = strfind(linedata,' ');
+idx         = idx(5) + 1;
+apf_data.phase_name = linedata(idx:end);
+
+linedata	= fgetl(fid);
+idx         = strfind(linedata,' ');
+apf_data.num_hkl    = str2double(linedata(1:idx(1)));
+
+for i = 1:1:apf_data.num_hkl
+    linedata	= fgetl(fid);
+    idx = strfind(linedata,'   ');
+    h   = str2double(linedata(1:idx(1)));
+    k   = str2double(linedata(idx(1):idx(2)));
+    l   = str2double(linedata(idx(2):idx(3)));
+    apf_data.pf(i).hkl  = [h k l];
+    
+    linedata    = fgetl(fid);
+    idx = strfind(linedata, ' <-  MEPSUM');
+    apf_data.pf(i).num_pts  = str2double(linedata(1:idx(1)));
+    pfdata  = zeros(apf_data.pf(i).num_pts, 11);
+    for j = 1:1:apf_data.pf(i).num_pts
+        linedata    = fgetl(fid);
+        pfdata(j,:) = str2num(linedata);
+    end
+    apf_data.pf(i).polar_angle  = pfdata(:,1);
+    apf_data.pf(i).azi_angle    = pfdata(:,2);
+    apf_data.pf(i).value_from_exp   = pfdata(:,3);
+    apf_data.pf(i).value_from_odf   = pfdata(:,4);
+    apf_data.pf(i).prog_number      = pfdata(:,5);
+    apf_data.pf(i).weight           = pfdata(:,6);
+    
+    apf_data.pf(i).omega        = pfdata(:,7);
+    apf_data.pf(i).chi          = pfdata(:,8);
+    apf_data.pf(i).phi          = pfdata(:,9);
+    apf_data.pf(i).eta          = pfdata(:,10);
+    apf_data.pf(i).bank_num     = pfdata(:,11);
+end
+fclose(fid);
