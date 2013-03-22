457b4d16790578c49ef365ef8f3f0a1a787ac98e
Joel Bernier <joelvbenier@me.com>
2013-03-22 17:30:43 -0500

    angle conversion (rad <> deg)
    replaces maptool box functions
    
    
    Former-commit-id: 4c815f270b475bfbed29b30c4ddcfcdb3bb93e32

---
+++ conversion/deg2rad.m
@@ -0,0 +1,21 @@
+function rad = deg2rad(deg)
+% deg2rad - converts degrees to radians
+%
+%   USAGE:
+%
+%   rad = deg2rad(deg)
+%
+%   INPUT:
+%
+%   rad
+%       angle in radians
+%
+%   OUTPUT:
+%
+%   deg
+%       angle in degrees
+%
+%   NOTE:
+%
+%   This function exists in the matlab map toolbox
+rad = deg*pi/180;
---
+++ conversion/rad2deg.m
@@ -0,0 +1,21 @@
+function deg = rad2deg(rad)
+% rad2deg - converts radians to degrees
+%
+%   USAGE:
+%
+%   deg = rad2deg(rad)
+%
+%   INPUT:
+%
+%   deg
+%       angle in degrees
+%
+%   OUTPUT:
+%
+%   deg
+%       angle in r
+%
+%   NOTE:
+%
+%   This function exists in the matlab map toolbox
+deg = rad*180/pi;
