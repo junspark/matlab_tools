60cb73fe7b495b23a7b6f44d227a6146a2eaab2d
Jun-Sang Park <jp118@cornell.edu>
2014-03-22 15:34:18 -0500

    mesh generator for polar binning
    Former-commit-id: f5d9e20d37c5a048e3e414176d6e6030b897aa90

--- xrd-powder/data-reduction/mesh/BuildMeshDetector.m
+++ xrd-powder/data-reduction/mesh/BuildMeshDetector.m
@@ -1,9 +1,9 @@
-function mesh=buildMeshXRD(imageData)
-L=size(imageData,2);
+function mesh = BuildMeshDetector(L)
+% L=size(imageData,2);
 con=zeros(3,(L-1)*2*(L-1));
 
 
-for j=1:size(imageData,2)-1
+for j=1:L-1
     con(:,1+(L*2-2)*(j-1):(L*2-2)*j)=[[L*(j-1)+1 reshape(repmat((L*(j-1)+2):(L*(j-1)+L-1),2,1),1,(L-2)*2)  L*j];...
         reshape([(L*(j-1)+2):(L*j); (L*j+2):(L*j+L)],1,(L-1)*2);...
         reshape(repmat((L*j+1):(L*j+L-1),2,1),1,(L-1)*2)];
