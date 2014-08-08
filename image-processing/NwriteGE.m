90de948cd7fb5b14e3d4ccb12357401e14d9b007
Jun-Sang Park <jp118@cornell.edu>
2014-08-08 12:51:10 -0500

    NwriteGE writes multiple frames
    Former-commit-id: 02ed467d820e9690bbd14356285f21238a147d10

--- image-processing/NwriteGE.m
+++ image-processing/NwriteGE.m
@@ -7,17 +7,18 @@ function status = NwriteGE(pfname, data)
 %       name of the GE image full file name
 %
 %   data
-%       data to write
+%       data to write. 2048 x 208 x n where n is the number of frames
 %
 %   OUTPUT:
 %
 %   status
 %       status of the file write
 %
-%   NOTE:
-%   1. only writes 1 frame for now
 
-fid     = fopen(pfname, 'w');
+nFrames = size(data, 3);
+fid = fopen(pfname, 'w');
 status  = fwrite(fid, zeros(4096,1), 'uint16');
-status  = fwrite(fid, data, 'uint16');
+for i = 1:1:nFrames
+    status  = fwrite(fid, data(:,:,i), 'uint16');
+end
 status  = fclose(fid);
