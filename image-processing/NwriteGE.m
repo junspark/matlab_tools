b4e4633f55cf4d9577c327e028e3967ce28e2afa
Jun-Sang Park <jp118@cornell.edu>
2014-08-25 17:13:17 -0500

    corrected the comments
    Former-commit-id: 83a6eb5fc681fb11e5c87a7b50c0a03c2ac3f8a2

--- image-processing/NwriteGE.m
+++ image-processing/NwriteGE.m
@@ -7,7 +7,7 @@ function status = NwriteGE(pfname, data)
 %       name of the GE image full file name
 %
 %   data
-%       data to write. 2048 x 208 x n where n is the number of frames
+%       data to write. 2048 x 2048 x n where n is the number of frames
 %
 %   OUTPUT:
 %
