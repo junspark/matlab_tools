93bcc64d4579603da0ce502bd42cab9b8e9a4690
Jun-Sang Park <jp118@cornell.edu>
2014-08-27 10:24:40 -0500

    coloring scheme for ff-hedm
    Former-commit-id: f7dab66a5ed021f9ce781303dc114cf0fdb62c06

--- ff-hedm/PlotGrains.m
+++ ff-hedm/PlotGrains.m
@@ -2,17 +2,44 @@ clear all
 close all
 clc
 
+%%% GENERATE IPF COLORMAP USING MTEX
+%%% FIRST RUN startup_mtex.m
+%%% CHECK IN ebsdColorbar.m
+cs  = symmetry('m-3m');
+ss  = symmetry('-1');
+cc  = get_option('antipodal','colorcoding','ipdfHKL');
+
+% [minTheta,maxTheta,minRho,maxRho,v] = getFundamentalRegionPF(cs, 'antipodal');
+% h   = S2Grid('PLOT', 'minTheta', minTheta, 'maxTheta', maxTheta,...
+%     'minRho', minRho, 'maxRho', maxRho, 'RESTRICT2MINMAX', 'resolution', 1*degree, 'antipodal');
+% v   = vector3d(h);
+% x   = getx(v); x = x(:);
+% y   = gety(v); y = y(:);
+% z   = getz(v); z = z(:);
+% d   = orientation2color(h,cc,cs,'antipodal');
+% save('coloring_scheme.mat', 'x', 'y', 'z', 'd')
+
+% create an EBSD variable containing the data
+% DATA
 % Sp_ID O[0][0] O[0][1] O[0][2] O[1][0] O[1][1] O[1][2] O[2][0] O[2][1] O[2][2] X Y Z a b c alpha beta gamma Err1 Err2 Err3 MeanRadius Confidence 
-qsym    = CubSymmetries;
 grains	= load('.\grains_example.csv');
 
-nGrains    = size(grains, 1);
+nGrains     = size(grains, 1);
 for i = 1:1:nGrains
     RMats(:,:,i)   =  reshape(grains(i,2:10), 3, 3)';
 end
+qsym    = CubSymmetries; Rsym    = RMatOfQuat(qsym);
 quat    = ToFundamentalRegionQ(QuatOfRMat(RMats), qsym);
 rod     = RodOfQuat(quat);
 
+ori     = orientation('quaternion', quat(1,:), quat(2,:), quat(3,:), quat(3,:), cs);
+ebsd    = EBSD(ori, cs, ss);
+rgb     = orientation2color(ori, 'ipdfHSV');
+
+plot(ebsd, 'colorcoding','ipdfHSV')
+
+quat    = ToFundamentalRegionQ(QuatOfRMat(RMats), qsym);
+rod     = RodOfQuat(quat);
 
 %%% THRESHOLDING BY COMPLETENESS
 Thresh_Completeness = 0.7;
@@ -32,6 +59,11 @@ figure, scatter3(grains(:,11), grains(:,12), grains(:,13), 50, grains(:,24), 'fi
 grid on
 axis square
 
+%%%% PLOT COM / RGB IN FUNDAMENTAL TRIANGLE AS IPDF
+figure, scatter3(grains(:,11), grains(:,12), grains(:,13), 50, rgb, 'filled') %% COMPLETENESS
+grid on
+axis square
+
 %%%% PLOT ORIENTATIONS / ONE COLOR
 figure, PlotFRPerimeter('cubic');
 scatter3(rod(1,:), rod(2,:), rod(3,:), 50, 'filled', 'b')
@@ -42,3 +74,8 @@ figure, PlotFRPerimeter('cubic');
 scatter3(rod(1,:), rod(2,:), rod(3,:), 50, grains(:,24), 'filled')
 axis square tight off
 colorbar vert
+
+%%%% PLOT ORIENTATIONS / RGB IN FUNDAMENTAL TRIANGLE AS IPDF
+figure, PlotFRPerimeter('cubic');
+scatter3(rod(1,:), rod(2,:), rod(3,:), 50, rgb, 'filled')
+axis square tight off
