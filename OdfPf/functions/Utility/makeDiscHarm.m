a8a278637910b996f5338063c9784d83179faaa7
Jun-Sang Park <jp118@cornell.edu>
2015-01-21 12:47:13 -0600

    duplicate functionality deleted
    replaced by DiscreteHarmonics
    
    
    Former-commit-id: 97d06f2741f880d3e4902e1172ee1b70d8c3254b

--- OdfPf/functions/Utility/makeDiscHarm.m
+++
@@ -1,8 +0,0 @@
-function [V, D] = makeDiscHarm(h1sip, l2ip, numh); 
-% [V, D] = makeDiscHarm(h1sip, l2ip, numh);
-  
-  [V, D] = eigs(h1sip, l2ip, numh, 'sm');
-  [D, ind] = sort(diag(D));
-  V = V(:, ind);
-  
-  
