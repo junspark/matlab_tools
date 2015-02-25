6882ab469a2029ad1f9c897d1ea576d766d9aec4
Jun-Sang Park <jp118@cornell.edu>
2015-02-25 13:50:22 -0600

    updates to par file reader to reflect new format
    Former-commit-id: 48dea0a6bd6ab420f059159b9fc981244e874364

--- parfile-processing/ReadPythonParFile.m
+++ parfile-processing/ReadPythonParFile.m
@@ -6,31 +6,52 @@ function pardata = ReadPythonParFile(fname, numchs)
 %   fname
 %       name of the python generated par file name
 %
-%   numchs
-%       number of channels recorded in the python generated par file (excluding the
-%       date / time channel)
-%
 %   OUTPUT:
 %
 %   pardata
 %       strip chart data in struct arrary.
-fmtstring       = '%s %s %d %s %d';
-for i = 1:1:numchs
-    fmtstring   = [fmtstring, ', %f'];
-end
+fmtstring       = '%s %s %d %s %d, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %s %s %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f';
 
 fid     = fopen(fname, 'r','n');
 header  = fgetl(fid);
 textdata  = textscan(fid, fmtstring);
 fclose(fid);
 
-pardata.hdr     = header;
-pardata.day     = textdata{1};
-pardata.month   = textdata{2};
-pardata.date    = textdata{3};
-pardata.time    = textdata{4};
-pardata.year    = textdata{5};
-pardata.chs     = zeros(length(pardata.date), numchs);
-for i = 1:1:numchs
-    pardata.chs(:,i)  = textdata{i+5};
-end
+pardata.hdr                 = header;
+pardata.day                 = textdata{1};
+pardata.month               = textdata{2};
+pardata.date                = textdata{3};
+pardata.time                = textdata{4};
+pardata.year                = textdata{5};
+pardata.Iring               = textdata{6};
+pardata.ElapsedTime         = textdata{7};
+pardata.ic0                 = textdata{8};
+pardata.ic1                 = textdata{9};
+pardata.NULL                = textdata{10};
+pardata.HorzInstDeadTime    = textdata{11};
+pardata.HorzAveDeadTime     = textdata{12};
+pardata.HorzRealElapTime    = textdata{13};
+pardata.HorzLiveElapTime    = textdata{14};
+pardata.VertInstDeadTime    = textdata{15};
+pardata.VertAveDeadTime     = textdata{16};
+pardata.VertRealElapTime    = textdata{17};
+pardata.VertLiveElapTime    = textdata{18};
+pardata.IterationNumber     = textdata{19};
+pardata.PositionNumber      = textdata{20};
+pardata.FileIDNumber        = textdata{21};
+pardata.HorzFileName        = textdata{22};
+pardata.VertFileName        = textdata{23};
+pardata.samX                = textdata{24};
+pardata.samY                = textdata{25};
+pardata.samY2               = textdata{26};
+pardata.samZ                = textdata{27};
+pardata.HorzSlitB           = textdata{28};
+pardata.HorzSlitT           = textdata{29};
+pardata.HorzSlitI           = textdata{30};
+pardata.HorzSlitO           = textdata{31};
+pardata.HorzTh              = textdata{32};
+pardata.VertSlitB           = textdata{33};
+pardata.VertSlitT           = textdata{34};
+pardata.VertSlitI           = textdata{35};
+pardata.VertSlitO           = textdata{36};
+pardata.VertTh              = textdata{37};
