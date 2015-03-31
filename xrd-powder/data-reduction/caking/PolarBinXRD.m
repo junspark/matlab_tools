5a373c3b82b4aa81f2709182658ec4d988f353fe
Jun-Sang Park <jp118@cornell.edu>
2015-03-31 14:08:40 -0500

    clarification of coordintes
    Former-commit-id: 39acfef9ed2e36bf2276e3e59a8f6f624d2ea34d

--- xrd-powder/data-reduction/caking/PolarBinXRD.m
+++ xrd-powder/data-reduction/caking/PolarBinXRD.m
@@ -1,9 +1,9 @@
-function polImg = PolarBinXRD(mesh, instr, cakeParms, img)
+function polimg = PolarBinXRD(mesh, instr, cakeParms, img)
 % PolarBinXRD - Polar integration of image data
 %
 %   USAGE:
 %
-%   polImg = PolarBinXRD(mesh, instr, cakeParms, img)
+%   polimg = PolarBinXRD(mesh, instr, cakeParms, img)
 %
 %   INPUT:
 %
@@ -21,21 +21,22 @@ function polImg = PolarBinXRD(mesh, instr, cakeParms, img)
 %
 %   OUTPUT:
 %
-%   polImg
+%   polimg
 %       radially integrated data organized in struct variable
 
 % save('PolarBinXRD_input.mat')
 % return
-
 % clear all
 % close all
 % clc
 % load('PolarBinXRD_input.mat')
+% cakeParms.fastint   = 1;
+% cakeParms.sector(3) = 240;
+% cakeParms.sector(4) = 272;
+% cakeParms.bins(2)   = 5;
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-img   = double(img); imgi   = img;
-% imgi  = rot90(fliplr(img), 1);
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+img     = double(img); 
+imgi    = img;
 
 L   = instr.detectorsize/instr.pixelsize;
 
@@ -43,11 +44,17 @@ L   = instr.detectorsize/instr.pixelsize;
 x0  = cakeParms.origin(1);   % in pixels
 y0  = cakeParms.origin(2);   % in pixels
 
+x0plt   = x0;
+y0plt   = instr.numpixels - y0;
+
 figure(1000)
-imagesc(log(abs(rot90(img,1))))
+% imagesc(log(abs(rot90(img,1))))
+imagesc(rot90(img,1))
 hold on
 axis equal tight
-plot(x0, y0, 'r*')
+plot(x0plt, y0plt, 'rh')
+xlabel('X_L (pixels)')
+ylabel('Y_L (pixels)')
 
 startAzi    = cakeParms.sector(1);
 endAzi      = cakeParms.sector(2);
@@ -70,40 +77,45 @@ R   = repmat(R, numEta + 1, 1)';
 
 Rlist   = mean((R(1:end - 1,:) + R(2:end,:))./2,2);
 
-polImg.azimuth   = cakeParms.azim;
-polImg.radius    = zeros(numRho, numAzi);
-polImg.intensity = zeros(numRho, numAzi);
+polimg.azimuth   = cakeParms.azim;
+polimg.radius    = zeros(numRho, numAzi);
+polimg.intensity = zeros(numRho, numAzi);
 
 for ii = 1:1:numAzi
     fprintf('Processing sector %d of %d\n', ii, numAzi);
     tic;
     
-    azi_ini = polImg.azimuth(ii) - dAzi/2;
-    azi_fin = polImg.azimuth(ii) + dAzi/2;
+    azi_ini = polimg.azimuth(ii) - dAzi/2;
+    azi_fin = polimg.azimuth(ii) + dAzi/2;
     
     TH  = azi_ini:dEta:azi_fin;
     TH  = repmat(TH, numRho + 1, 1);
     
-    [x, y]  = pol2cart(deg2rad(TH),R);
-    x       = x0 + x;
-    y       = y0 + y;
+    [x, y]	= pol2cart(deg2rad(TH),R);
+    x   = x0 + x; y   = y0 + y;
+    
+    THplt   = azi_ini:dEta:azi_fin;
+    THplt   = repmat(-THplt, numRho + 1, 1);
+    
+    [xplt, yplt]    = pol2cart(deg2rad(THplt),R);
+    xplt    = x0 + xplt; yplt    = y0plt + yplt;
 
     figure(1000)
-    title(num2str(polImg.azimuth(ii)))
-    plot(x, y, 'k.')
-
-    % tic
+    title(num2str(polimg.azimuth(ii)))
+    plot(xplt, yplt, 'k.')
+    
+    tic
     V   = zeros(numRho + 1, numEta + 1);
     for i = 1:1:(numRho + 1)
         for j = 1:1:(numEta + 1)
             xy  = [x(i,j); y(i,j)];
-            
             V(i,j) = DataCoordinates(xy, L, mesh, imgi);
+            
             %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
             % SOME BENCHMARK TESTS
             % gridNum     = (L - 1)*(fix(xy(2)) - 1) + fix(xy(1));
             % elemNums    = [gridNum*2-1 gridNum*2];
-            % 
+            %
             % P1  = mesh.crd(:, mesh.con(:,elemNums(1)));
             % P2  = mesh.crd(:, mesh.con(:,elemNums(2)));
             % IN1 = inpolygon(xy(1), xy(2), P1(1,:), P1(2,:));
@@ -139,14 +151,14 @@ for ii = 1:1:numAzi
         Ilist   = V;
     end
     
-    polImg.radius(:,ii)    = Rlist';
-    polImg.intensity(:,ii) = Ilist';
+    polimg.radius(:,ii)    = Rlist';
+    polimg.intensity(:,ii) = Ilist';
     
     dtime   = toc;
     fprintf('Processing time for sector %d is %1.4f\n', ii, dtime);
 end
 
-polImg.radius       = polImg.radius';
-polImg.intensity    = polImg.intensity';
+polimg.radius       = polimg.radius';
+polimg.intensity    = polimg.intensity';
 
-close(figure(1000))
+% close(figure(1000))
