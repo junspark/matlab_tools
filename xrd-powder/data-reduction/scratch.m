d458c04bd970174955c95d8b3db1c4d660532c99
Jun-Sang Park <jp118@cornell.edu>
2015-03-31 14:11:21 -0500

    scratch
    Former-commit-id: b03464dd6e49aa615f39ea2a30056d8405fdfbd1

---
+++ xrd-powder/data-reduction/scratch.m
@@ -0,0 +1,13 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% rotation for GE1 is 152.5
+% XRDIMAGE.Image.RotAngle     = 152.5;
+% XRDIMAGE.Image.RotAngle     = 67.5;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%         imgi    = imrotate(imgi, XRDIMAGE.Image.RotAngle);
+%         imgi    = [nan(1040, size(imgi,2)); imgi];
+%         imgi    = [nan(1040, size(imgi,2)); imgi];
+%         imgi    = [nan(size(imgi,1), 520) imgi nan(size(imgi,1), 520)];
+
+%         imgi    = [imgi, zeros(size(imgi,1), 1040)];
+%         imgi    = [zeros(520, size(imgi,2)); imgi; zeros(520, size(imgi,2))];
