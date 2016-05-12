e6ce55556d63a9b402411bccc7ca7a6e2d3ae078
Jun-Sang Park <jp118@cornell.edu>
2016-05-11 20:32:57 -0500

    outputs d spacings
    Former-commit-id: 30de67bb3ee26555399138bfb076890aee57f2e8

--- image-processing/MapIntensityToTThGrid.m
+++ image-processing/MapIntensityToTThGrid.m
@@ -1,4 +1,4 @@
-function [tth_grid, intensity_in_tth_grid] = MapIntensityToTThGrid(XRDIMAGE, polimg)
+function [tth_grid, d_grid, intensity_in_tth_grid] = MapIntensityToTThGrid(XRDIMAGE, polimg)
 % MapIntensityToTThGrid - Apply geometric model and detector distortion to
 % generate intensity vs. tth data in a standard tth grid
 
@@ -12,6 +12,7 @@ function [tth_grid, intensity_in_tth_grid] = MapIntensityToTThGrid(XRDIMAGE, pol
 tth_min = max(polimg.mapped_tth_for_intensity(:,1));
 tth_max = min(polimg.mapped_tth_for_intensity(:,end));
 tth_grid    = linspace(tth_min, tth_max, XRDIMAGE.CakePrms.bins(2));
+d_grid      = (XRDIMAGE.Instr.wavelength/2)./sind(tth_grid./2);
 
 % intensity_in_tth_grid   = zeros(XRDIMAGE.CakePrms.bins(1), XRDIMAGE.CakePrms.bins(2));
 % for i = 1:1:XRDIMAGE.CakePrms.bins(1)
