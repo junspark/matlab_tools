f547a676328c199785f73780f4f2fda490d843b6
Jun-Sang Park <jp118@cornell.edu>
2016-06-10 15:37:37 -0500

    dexela reader
    Former-commit-id: a45ffb7693be379f9df44eb6d3e5fc3654941c99

---
+++ image-processing/NreadDexela.m
@@ -0,0 +1,30 @@
+function img = NreadDexela(pfname, frameno)
+% NreadDexela - read Dexela file from fastsweep image stack saved in ncdf
+% format (nc extension).
+%
+%   INPUT:
+%
+%   filename
+%       name of the Dexela image stack file name
+%
+%   frameno
+%       frame to load
+%
+%   OUTPUT:
+%
+%   img
+%       image data in a [3072 x 3888] matrix in default size - 1 x 1 binning
+%
+
+finfo       = ncinfo(pfname);
+nframe      = finfo.Dimensions(1).Length;
+numpixHorz  = finfo.Dimensions(2).Length;
+numpixVert  = finfo.Dimensions(3).Length;
+
+if frameno > nframe
+    error('frame number %d requested is larger than number of frames %d stored in the file %s', frameno, nframe, pfname);
+else
+    istart  = [1 1 frameno]';
+    iend    = [numpixVert numpixHorz 1]';
+    img     = ncread(pfname, 'array_data', istart, iend);
+end
