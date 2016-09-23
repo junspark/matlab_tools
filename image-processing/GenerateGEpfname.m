e4df53e770b668d9e11ab3fdce5adc81b265a562
Jun-Sang Park <jp118@cornell.edu>
2016-09-23 10:12:14 -0500

    ge file names can have more digits
    Former-commit-id: 7e373e1fe6090e3ad644fc3b26728f2c0f014bd9

--- image-processing/GenerateGEpfname.m
+++ image-processing/GenerateGEpfname.m
@@ -22,14 +22,16 @@ if ~isfield(ImagePars, 'iscor') || ~ImagePars.iscor
     numimages   = length(ImagePars.fnumber);
     
     for i = 1:1:numimages
-        fname   = sprintf([ImagePars.fbase, '%05d.%s'], ImagePars.fnumber(i), ImagePars.fext);
+        % fname   = sprintf([ImagePars.fbase, '%05d.%s'], ImagePars.fnumber(i), ImagePars.fext);
+        fname   = sprintf([ImagePars.fbase, '%0', num2str(ImagePars.numdigs), 'd.%s'], ImagePars.fnumber(i), ImagePars.fext);
         pfname{i,1} = fullfile(ImagePars.pname, fname);
     end
 else
     numimages   = length(ImagePars.fnumber);
     
     for i = 1:1:numimages
-        fname_base  = sprintf([ImagePars.fbase, '%05d.%s'], ImagePars.fnumber(i), ImagePars.fext);
+        % fname_base  = sprintf([ImagePars.fbase, '%05d.%s'], ImagePars.fnumber(i), ImagePars.fext);
+        fname_base  = sprintf([ImagePars.fbase, '%0', num2str(ImagePars.numdigs), 'd.%s'], ImagePars.fnumber(i), ImagePars.fext);
         for j = 1:1:ImagePars.numframe
             fname   = sprintf([fname_base, '.frame%d.cor'], j);
             pfname{i,j} = fullfile(ImagePars.pname, fname);
