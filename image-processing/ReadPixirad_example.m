f0bc9a73136ffcd9a0b0bbcc38e7c1af7f6029c2
Jun-Sang Park <jp118@cornell.edu>
2017-02-21 16:38:45 -0600

    s1_pixirad2 support

--- image-processing/ReadPixirad_example.m
+++ image-processing/ReadPixirad_example.m
@@ -2,26 +2,51 @@ clear all
 close all
 clc
 
-pname   = 'C:\Users\parkjs\Documents\GitHub\matlab_tools_examples\image-processing-test-images';
-fname   = 'AgBeh_013043.tif';
+%%% FOR PIXI1
+% pname   = 'C:\Users\parkjs\Documents\GitHub\matlab_tools_examples\image-processing-test-images';
+% fname   = 'AgBeh_013043.tif';
+% 
+% pfname  = fullfile(pname, fname);
+% 
+% raw = imread(pfname);
+% % csq = ReadPixirad(pfname, 'nxsq', 900);
+% csq = ReadPixirad(pfname);
+% 
+% figure(1)
+% imagesc(log(double(raw)))
+% % imagesc(raw)
+% axis equal
+% 
+% figure(2)
+% imagesc(log(csq))
+% % imagesc(csq)
+% axis equal
+
+%%% FOR PIXI2
+pname   = '/home/beams/S1IDUSER/mnt/s1c/pixirad_feb17';
+% fname   = 'test_001995.tif';
+fname   = 'furnace_test_000030.tif';
 
 pfname  = fullfile(pname, fname);
 
 raw = imread(pfname);
-% csq = ReadPixirad(pfname, 'nxsq', 900);
-csq = ReadPixirad(pfname);
+csq = ReadPixirad(pfname, 'version', 'pixi2');
 
 figure(1)
-imagesc(log(double(raw)))
+imagesc(raw)
 % imagesc(raw)
-axis equal
+axis equal tight
 
 figure(2)
-imagesc(log(csq))
+imagesc(csq)
 % imagesc(csq)
-axis equal
-return
+axis equal tight
 
+% %%% CORRECTION TABLE FOR PIXI2
+fid = fopen('/home/beams/S1IDUSER/mnt/s1a/misc/pixirad2/usb.after_repair/Calibrations/2010.crrm', 'r');
+data    = fread(fid, 'float');
+fclose(fid);
+data    = reshape(data, 1024, 402);
 
-
-
+figure(3)
+imagesc(data)
