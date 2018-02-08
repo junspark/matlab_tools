9c097a4147e537da3d14aa13db6c4aff078e82e8
cpchuang <chuang.cp@gmail.com>
2018-02-08 01:05:27 -0600

    update edd reader to v1.9

--- edd-tools/readedd5_6bm.m
+++ edd-tools/readedd5_6bm.m
@@ -2,6 +2,9 @@
 %  read data into a single Matlab struct
 %  supported configuration: after Feb.2018
 %
+%  Rev.1.9 (2018/02/07)
+%  + read all motor information
+%
 %  Rev.1.8 (2018/01/18)
 %  + new format to combine det-1/det-2 data in one file
 %
@@ -30,7 +33,7 @@
 %  + read multiple scans from single file (append mode)
 %
 % Copyright 2015-2018 Andrew Chuang (chuang.cp@gmail.com)
-% $Revision: 1.8 $  $Date: 2018/01/18 $
+% $Revision: 1.9 $  $Date: 2018/02/07 $
 
 
 function [edd]=readedd5_6bm(logtoopen)
@@ -45,10 +48,10 @@ end
 if isdir(logtoopen)
     [fpath, fname, ~] = fileparts(logtoopen);
     fullname= fullfile(logtoopen,sprintf('%s.xy',fname));
-    fid=fopen(fullname);if fid == -1, error('Can''t find/open the input file.'); end;
+    fid=fopen(fullname);if fid == -1, error('Can''t find/open the input file.'); end
     step_counter = zeros(5000,1);
 else
-    fid=fopen(logtoopen);if fid == -1, error('Can''t open the input file.'); end;
+    fid=fopen(logtoopen);if fid == -1, error('Can''t open the input file.'); end
     step_counter = zeros(5000,1);
 end
 
@@ -64,10 +67,11 @@ fprintf('Reading data from %s... %3.0d scan set(s) found!\n',logtoopen,length(in
 % initialize data cell
 edd=struct('command',[],...
     'data',cell(1),...
+    'allmotors','',...
+    'motornames','',...    
     'motorname','',...
     'motorpos',[],...
     'motorpos_all',[],...
-    'keyence',[],...
     'motor_start_end_numstep',[],...
     'motorstep',[],...
     'exp_time',[],...
@@ -88,7 +92,7 @@ for i = 1:length(ind_command)
     scanen = [scanst(2:end)-1 length(snlog)];
     % find first few line feed
     search_limit = min(1000,length(snlog));  % first 1000 characters should cover!!
-    lf = find(snlog(1:search_limit) == char(10));
+    lf = find(snlog(1:search_limit) == newline);  % newline = char(10)
     % find comment
     %    comment = strfind(snlog,'#');
     command_= snlog(1:lf(1)-1);
@@ -97,7 +101,16 @@ for i = 1:length(ind_command)
     
     edd(i).command = command_(11:end);
     
-    variable_used = 0;  % assume no variable is used for 
+    variable_used = 0;  % assume no variable is used to do motor_cam
+    
+    % name of all motors
+    motor_name_all = strsplit(snlog(lf(1)+3:lf(2)-2),' ');
+    % value of all motors
+    motor_value_all = str2num(snlog(lf(2)+3:lf(3)-2));
+    % convert all motor information into struct
+    edd(i).allmotors = cell2struct(num2cell(motor_value_all),motor_name_all,2);
+        
+    %assignin('base','tmp',motor_all);
     
     switch cmd_use
         case 'motor_cam'
@@ -106,10 +119,13 @@ for i = 1:length(ind_command)
             mend    = str2num(chk_cmd{6});
             numstep = str2num(chk_cmd{7});  % number steps in command  
             exptime = str2num(chk_cmd{8});
+
             %%%%% if length([mstart mend mstep exptime])~=4
             % check if the scan command was finished correctly.
             nosteps = length(scanst);  % number steps recorded in file
-            if nosteps ~= numstep
+            if isempty(numstep)
+                warning('Variable used. Can not check if scan complete or not!!')
+            elseif nosteps ~= numstep
                 warning('Scans may be interrupted!!')
             end
             % if variable is used in script, following parameters might not
@@ -119,14 +135,15 @@ for i = 1:length(ind_command)
             if isempty(numstep); numstep= 0; variable_used = 1; end
             if isempty(exptime); exptime= 0; variable_used = 1; end            
             
-            edd(i).log       = snlog(1:lf(3));
+            %edd(i).log       = snlog(1:lf(3));
+            edd(i).log       = snlog(1:lf(1));
             edd(i).motorname = mname;
             edd(i).motor_start_end_numstep = [mstart mend numstep];
             edd(i).motorstep = (mend-mstart)/(numstep-1);
             edd(i).exp_time  = exptime;            
             edd(i).data{1}   = zeros(nosteps,8192);
             edd(i).motorpos  = zeros(1,nosteps);
-            edd(i).keyence   = zeros(1,nosteps);            
+%            edd(i).keyence   = zeros(1,nosteps);            
             
         case 'motor_cam2'
             %%%%% To-do %%%%%
@@ -158,7 +175,7 @@ for i = 1:length(ind_command)
             edd(i).exp_time  = exptime;            
             edd(i).data      = zeros(nosteps,8192);
             edd(i).motorpos  = zeros(1,nosteps);
-            edd(i).keyence   = zeros(1,nosteps);            
+%            edd(i).keyence   = zeros(1,nosteps);            
                         
         otherwise
             fprintf('Abort!!\n This command "%s" is not supported by this reader yet!!\n',cmd_use);
@@ -172,42 +189,40 @@ for i = 1:length(ind_command)
         for j = 1:nosteps
             tmpda = snlog(scanst(j):scanen(j));   % get data of single scan
             %comment = strfind(tmp_,'#')
-            lf = find(tmpda == char(10));         % find line feed
+            lf = find(tmpda == newline);         % find line feed
             % header
             % header_1 = tmpda(1:lf(1)-1)
             % header_2 = tmpda(lf(1)+1:lf(2)-1)
             header_3 = tmpda(lf(2)+1:lf(3)-1);
             
-            assignin('base','tmp',header_3);
+            %assignin('base','tmp',header_3);
             
-            h3data = sscanf(header_3,'%*s %*s %*s %*s %*s %*s %*s %*s %*s %*s %*s %f %f %f %f %f %f %f %f %*f %f'); % read only keyence value
-            % if keyence data exist, read it!!
-            if length(h3data)==9
-                edd(i).keyence(j) = h3data(9);
+            separator = find(header_3=='=');
+            % read all motor value
+            h3data = str2num(header_3(separator+1:end));
+
+            % save all motor position
+            edd(i).motorpos_all(j,:) = h3data;
+            
+            % suppose same motor name saved for every step, save motorname only once
+            if j==1
+                edd(i).motornames = strsplit(header_3(2:separator-2));
+                % find which motor is moving motor
+                motor_no = find(~cellfun(@isempty,strfind(edd(i).motornames,edd(i).motorname)));
+
+                if isempty(motor_no)
+                    fprintf('\n"%s" position is not saved!!\n',edd(i).motorname)
+                end
             end
-            % save xr yr zr (keyence) position
-            edd(i).motorpos_all(:,j) = h3data';
-            % reassign motorposition based on moving motor
-            switch edd(i).motorname
-                case 'xr'
-                    edd(i).motorpos(j) = h3data(1);
-                case 'yr'
-                    edd(i).motorpos(j) = h3data(2);
-                case 'zr'
-                    edd(i).motorpos(j) = h3data(3);
-                case 'roty'
-                    edd(i).motorpos(j) = h3data(4);
-                case 'ksamx'
-                    edd(i).motorpos(j) = h3data(5);
-                case 'ksamz'
-                    edd(i).motorpos(j) = h3data(6);
-                case 'ksamrx'
-                    edd(i).motorpos(j) = h3data(7);
-                case 'ksamrz'
-                    edd(i).motorpos(j) = h3data(8);
-                otherwise
-                    fprintf('\nmotor_name "%s" not supported yet!!\n',edd(i).motorname)
+            
+            % assign motor position based on moving motor            
+            if isempty(motor_no)
+                edd(i).motorpos(j) = 0;
+            else
+                 edd(i).motorpos(j) = h3data(motor_no);
             end
+            
+            %motor_no = find(~cellfun(@isempty,strfind(edd(i).motornames,edd(i).motorname)))
             
             % if variable is used, calculate motor_step again from real data.
             if  variable_used
@@ -218,12 +233,12 @@ for i = 1:length(ind_command)
             if rem(length(lf),8195) ~= 0
                 warning('Scan %d may be interrupted!!',j)
             end
-            da = sscanf(tmpda(lf(3)+1:end),'%f');  % read the data
+            da = sscanf(tmpda(lf(3)+1:end),'%f');  % read the detector data
             numcol = length(da)/8192;              % number of column
             for k = 2:numcol
                 edd(i).data{k-1}(j,:) = da(k:numcol:end);     % read k-th detector data
             end
-            edd(i).log = [edd(i).log tmpda(1:lf(3))];
+            edd(i).log = [edd(i).log tmpda(1:lf(2))];
             
             step_counter(j)=step_counter(j)+1;
         end
