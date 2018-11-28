9963403696661b3c429b3db4c2c45cbb1b8329f3
Jun-Sang Park <jp118@cornell.edu>
2018-11-28 13:23:08 -0600

    updates to GE frame calculator

--- image-processing/CalcNumFramesGE.m
+++ image-processing/CalcNumFramesGE.m
@@ -1,4 +1,23 @@
 function num_frames = CalcNumFramesGE(pfname)
+% CalcNumFramesGE - Calculates the numbers of frames in a GE file with
+% multiple frames.
+%
+%   USAGE:
+%
+%   num_frames = CalcNumFramesGE(pfname)
+%
+%   INPUT:
+%
+%   pfname          path where GE file is located.
+%
+%   OUTPUT:
+%
+%   num_frames      number of frames in the file.
 
-num_frames  = dir(pfname);
-num_frames  = (num_frames.bytes-8192)/2048/2048/2;
+if exist(pfname, 'file')
+    num_frames  = dir(pfname);
+    num_frames  = (num_frames.bytes-8192)/2048/2048/2;
+else
+    disp(sprintf('%s does not exist.', pfname))
+    num_frames  = -1;
+end
