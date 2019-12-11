60b1e05b4668ca1a92d0872ad155f3e85f9bbec4
Jun-Sang Park <parkjs@pinotnoir.xray.aps.anl.gov>
2019-12-11 08:29:11 -0600

    edd reader from andrew chuang

--- image-processing/GenerateGEpfname.m
+++ image-processing/GenerateGEpfname.m
@@ -1,4 +1,4 @@
-function pfname = GenerateGEpfname(ImagePars)
+function pfname = GenerateGEpfname(ImagePars, varargin)
 % GenerateGEpfname - generate file names for GE image files
 %
 %   USAGE:
@@ -12,14 +12,29 @@ function pfname = GenerateGEpfname(ImagePars)
 %       patterns. Must contain pname (path name), fbase (file name), fnumber(series of file
 %       number), fext (file extension, i.e ge1)
 %
+%   These arguments can be followed by a list of
+%   parameter/value pairs which control certain plotting
+%   features.  Options are:
+%
+%   NewFileNameFormat   Use old file naming convenction where "_" was 
+%                       included in the file name prefix (default is new). This
+%                       is to ensure backwards compatibility.
+%
 %   OUTPUT:
 % 
 %   pfname
 %       cell structure with the full file names
 %
 
-%%% Edits by Connor Horn on 7/20/18 to adapt to .cor32 format
+% DEFAULT OPTIONS
+optcell = {...
+    'NewFileNameFormat', true, ...
+    };
 
+% UPDATE OPTION
+opts    = OptArgs(optcell, varargin);
+
+%%% Edits by Connor Horn on 7/20/18 to adapt to .cor32 format
 iscor = isfield(ImagePars, 'iscor') && ImagePars.iscor;
 iscor32 = isfield(ImagePars, 'iscor32') && ImagePars.iscor32;
 
@@ -27,16 +42,23 @@ if ~iscor && ~iscor32
     numimages   = length(ImagePars.fnumber);
     
     for i = 1:1:numimages
-        % fname   = sprintf([ImagePars.fbase, '%05d.%s'], ImagePars.fnumber(i), ImagePars.fext);
-        fname   = sprintf([ImagePars.fbase, '%0', num2str(ImagePars.numdigs), 'd.%s'], ImagePars.fnumber(i), ImagePars.fext);
+        if opts.NewFileNameFormat
+            fname   = sprintf([ImagePars.fbase, '_%0', num2str(ImagePars.numdigs), 'd.%s'], ImagePars.fnumber(i), ImagePars.fext);
+        else
+            fname   = sprintf([ImagePars.fbase, '%0', num2str(ImagePars.numdigs), 'd.%s'], ImagePars.fnumber(i), ImagePars.fext);
+        end
         pfname{i,1} = fullfile(ImagePars.pname, fname);
     end
 elseif iscor
     numimages   = length(ImagePars.fnumber);
     
     for i = 1:1:numimages
-        % fname_base  = sprintf([ImagePars.fbase, '%05d.%s'], ImagePars.fnumber(i), ImagePars.fext);
-        fname_base  = sprintf([ImagePars.fbase, '%0', num2str(ImagePars.numdigs), 'd.%s'], ImagePars.fnumber(i), ImagePars.fext);
+        if opts.NewFileNameFormat
+            fname_base  = sprintf([ImagePars.fbase, '_%0', num2str(ImagePars.numdigs), 'd.%s'], ImagePars.fnumber(i), ImagePars.fext);
+        else
+            fname_base  = sprintf([ImagePars.fbase, '%0', num2str(ImagePars.numdigs), 'd.%s'], ImagePars.fnumber(i), ImagePars.fext);
+        end
+        
         for j = 1:1:ImagePars.numframe
             %fname   = sprintf([fname_base, '.frame%d.cor'], j);
             fname   = sprintf([fname_base, '_frame_%d.cor'], j); % edited by CH on 8/3/17 to reflect format of batchcorr output by adding a '.'
@@ -49,7 +71,12 @@ elseif iscor32
     numimages   = length(ImagePars.fnumber);
     
     for i = 1:1:numimages
-        fname_base  = sprintf([ImagePars.fbase, '%0', num2str(ImagePars.numdigs), 'd.%s'], ImagePars.fnumber(i), ImagePars.fext);
+        if opts.NewFileNameFormat
+            fname_base  = sprintf([ImagePars.fbase, '_%0', num2str(ImagePars.numdigs), 'd.%s'], ImagePars.fnumber(i), ImagePars.fext);
+        else
+            fname_base  = sprintf([ImagePars.fbase, '%0', num2str(ImagePars.numdigs), 'd.%s'], ImagePars.fnumber(i), ImagePars.fext);
+        end
+        
         for j = 1:1:ImagePars.numframe
             %fname   = sprintf([fname_base, '.frame.%d.cor32'], j);
             fname   = sprintf([fname_base, '_frame_%d.cor32'], j);
