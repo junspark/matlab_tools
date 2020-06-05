c25cdfeba9c30c4c2ae9509f5bb6f0ebc2af5f25
junspark <jun.sang.park@outlook.com>
2020-06-05 10:20:25 -0500

    vtkwriter updated to the correct location.

--- paraview/vtkwrite.m
+++ paraview/vtkwrite.m
@@ -15,20 +15,30 @@ function vtkwrite( filename,dataType,varargin )
 %  x,y,z,r must all be the same size and contain the corresponding position
 %  and scalar values. 
 %
-%  vtkwrite(filename,'structured_grid',x,y,z,'vectors',title1,u,v,w,'scalars',
+%  vtkwrite(filename,'structured_grid',x,y,z,'vectors',title,u,v,w,'scalars',
 %  title2,r) writes a 3D structured grid that contains both vector and scalar values.
 %  x,y,z,u,v,w,r must all be the same size and contain the corresponding
 %  positon, vector and scalar values.
 %
-%  vtkwrite(filename,'unstructured_grid',x,y,z,'vectors',title1,u,v,w,'scalars',
+%  vtkwrite(filename, 'structured_points', title, m) saves matrix m (could
+%  be 1D, 2D or 3D array) into vtk as structured points.
+%
+%  vtkwrite(filename, 'structured_points', title, m, 'spacing', sx, sy, sz)
+%  allows user to specify spacing. (default: 1, 1, 1). This is the aspect
+%  ratio of a single voxel. 
+%
+%  vtkwrite(filename, 'structured_points', title, m, 'origin', ox, oy, oz)
+%  allows user to speicify origin of dataset. (default: 0, 0, 0).
+%
+%  vtkwrite(filename,'unstructured_grid',x,y,z,'vectors',title,u,v,w,'scalars',
 %  title2,r) writes a 3D unstructured grid that contains both vector and scalar values.
 %  x,y,z,u,v,w,r must all be the same size and contain the corresponding
 %  positon, vector and scalar values.
 %  
-%  vtkwrite(filename,'polydata','lines',x,y,z) exports a 3D line where
-%  x,y,z are coordinates of the points that make the line. x,y,z are
+%  vtkwrite(filename, 'polydata', 'lines', x, y, z) exports a 3D line where
+%  x,y,z are coordinates of the points that make the line. x, y, z are
 %  vectors containing the coordinates of points of the line, where point(n)
-%  is specified by x(n),y(n) and z(n).
+%  is specified by x(n), y(n) and z(n).
 %
 %  vtkwrite(filename,'polydata','lines',x,y,z,'Precision',n) allows you to
 %  specify precision of the exported number up to n digits after decimal
@@ -47,8 +57,8 @@ function vtkwrite( filename,dataType,varargin )
 %  filename ''matlab_export.vtk' and automatically loads data into
 %  ParaView. 
 %  
-%  Version 2.2.0
-%  Chaoyuan Yeh, 2015
+%  Version 2.3
+%  Copyright, Chaoyuan Yeh, 2016
 %  Codes are modified from William Thielicke and David Gingras's submission.    
 
 if strcmpi(filename,'execute'), filename = 'matlab_export.vtk'; end
@@ -58,12 +68,62 @@ fid = fopen(filename, 'w');
 fprintf(fid, '# vtk DataFile Version 2.0\n');
 % 2. Title
 fprintf(fid, 'VTK from Matlab\n');
+
+
+binaryflag = any(strcmpi(varargin, 'BINARY'));
+if any(strcmpi(varargin, 'PRECISION'))
+    precision = num2str(varargin{find(strcmpi(vin, 'PRECISION'))+1});
+else
+    precision = '2';
+end
+
 switch upper(dataType)
+    case 'STRUCTURED_POINTS'
+        title = varargin{1};
+        m = varargin{2};
+        if any(strcmpi(varargin, 'spacing'))
+            sx = varargin{find(strcmpi(varargin, 'spacing'))+1};
+            sy = varargin{find(strcmpi(varargin, 'spacing'))+2};
+            sz = varargin{find(strcmpi(varargin, 'spacing'))+3};
+        else
+            sx = 1;
+            sy = 1;
+            sz = 1;
+        end
+        if any(strcmpi(varargin, 'origin'))
+            ox = varargin{find(strcmpi(varargin, 'origin'))+1};
+            oy = varargin{find(strcmpi(varargin, 'origin'))+2};
+            oz = varargin{find(strcmpi(varargin, 'origin'))+3};
+        else
+            ox = 0;
+            oy = 0;
+            oz = 0;
+        end
+        [nx, ny, nz] = size(m);
+        setdataformat(fid, binaryflag);
+        
+        fprintf(fid, 'DATASET STRUCTURED_POINTS\n');
+        fprintf(fid, 'DIMENSIONS %d %d %d\n', nx, ny, nz);
+        fprintf(fid, ['SPACING ', num2str(sx), ' ', num2str(sy), ' ',...
+            num2str(sz), '\n']);
+        fprintf(fid, ['ORIGIN ', num2str(ox), ' ', num2str(oy), ' ',...
+            num2str(oz), '\n']); 
+        fprintf(fid, 'POINT_DATA %d\n', nx*ny*nz);
+        fprintf(fid, ['SCALARS ', title, ' float 1\n']);
+        fprintf(fid,'LOOKUP_TABLE default\n');
+        if ~binaryflag 
+            spec = ['%0.', precision, 'f '];
+            fprintf(fid, spec, m(:)');
+        else
+            fwrite(fid, m(:)', 'float', 'b');
+        end
+        
     case {'STRUCTURED_GRID','UNSTRUCTURED_GRID'}
         % 3. The format data proper is saved in (ASCII or Binary). Use
         % fprintf to write data in the case of ASCII and fwrite for binary.
         if numel(varargin)<6, error('Not enough input arguments'); end
-        fprintf(fid, 'BINARY\n');
+        setdataformat(fid, binaryflag);
+%         fprintf(fid, 'BINARY\n');
         x = varargin{1};
         y = varargin{2};
         z = varargin{3};
@@ -78,12 +138,19 @@ switch upper(dataType)
         % Then the geomettry part describes geometry and topology of the dataset.
         if strcmpi(dataType,'STRUCTURED_GRID')
             fprintf(fid, 'DATASET STRUCTURED_GRID\n');
-            fprintf(fid, ['DIMENSIONS ' num2str(size(x,2)) ' ' num2str(size(x,1)) ' ' num2str(size(x,3)) '\n']);
+            fprintf(fid, 'DIMENSIONS %d %d %d\n', size(x,1), size(x,2), size(x,3));
         else
             fprintf(fid, 'DATASET UNSTRUCTURED_GRID\n');
         end
         fprintf(fid, ['POINTS ' num2str(n_elements) ' float\n']);
-        fwrite(fid, [x(:)';y(:)';z(:)'],'float','b');
+        output = [x(:)'; y(:)'; z(:)'];
+        
+        if ~binaryflag
+            spec = ['%0.', precision, 'f '];
+            fprintf(fid, spec, output);
+        else
+            fwrite(fid, output, 'float', 'b');
+        end
         % 5.This final part describe the dataset attributes and begins with the
         % keywords 'POINT_DATA' or 'CELL_DATA', followed by an integer number
         % specifying the number of points of cells. Other keyword/data combination
@@ -98,9 +165,19 @@ switch upper(dataType)
                 % Data enteries begin with a keyword specifying data type
                 % and numeric format.
                 fprintf(fid, ['\nVECTORS ', title,' float\n']);
-                fwrite(fid, [reshape(varargin{vidx(ii)+2},1,n_elements);...
-                reshape(varargin{vidx(ii)+3},1,n_elements);...
-                reshape(varargin{vidx(ii)+4},1,n_elements)],'float','b');
+                output = [varargin{ vidx(ii) + 2 }(:)';...
+                          varargin{ vidx(ii) + 3 }(:)';...
+                          varargin{ vidx(ii) + 4 }(:)'];
+
+                if ~binaryflag
+                    spec = ['%0.', precision, 'f '];
+                    fprintf(fid, spec, output);
+                else
+                    fwrite(fid, output, 'float', 'b');
+                end
+%                 fwrite(fid, [reshape(varargin{vidx(ii)+2},1,n_elements);...
+%                 reshape(varargin{vidx(ii)+3},1,n_elements);...
+%                 reshape(varargin{vidx(ii)+4},1,n_elements)],'float','b');
             end
         end
         if sidx~=0
@@ -108,11 +185,18 @@ switch upper(dataType)
                 title = varargin{sidx(ii)+1};
                 fprintf(fid, ['\nSCALARS ', title,' float\n']);
                 fprintf(fid, 'LOOKUP_TABLE default\n');
-                fwrite(fid, reshape(varargin{sidx(ii)+2},1,n_elements),'float','b');
+                if ~binaryflag
+                    spec = ['%0.', precision, 'f '];
+                    fprintf(fid, spec, varargin{ sidx(ii) + 2});
+                else
+                    fwrite(fid, varargin{ sidx(ii) + 2}, 'float', 'b');
+                end
+%                 fwrite(fid, reshape(varargin{sidx(ii)+2},1,n_elements),'float','b');
             end
         end
         
     case 'POLYDATA'
+
         fprintf(fid, 'ASCII\n');
         if numel(varargin)<4, error('Not enough input arguments'); end
         x = varargin{2}(:);
@@ -135,15 +219,15 @@ switch upper(dataType)
         end
         nbpoint = numel(x);
         fprintf(fid, ['POINTS ' num2str(nbpoint) ' float\n']);
-        precision = '3';
-        if any(strcmpi(varargin,'PRECISION'))
-            precision = num2str(uint8(varargin{find(strcmpi(varargin,'PRECISION'))+1}));
-            if str2double(precision) < 0, error('Invalid precision spec.');end
-        end
-        spec = [repmat(['%0.',precision,'f '],1,9),'\n'];
-        fprintf(fid,spec, [x(1:3:end-2) y(1:3:end-2) z(1:3:end-2) ...
-             x(2:3:end-1) y(2:3:end-1) z(2:3:end-1) ...
-             x(3:3:end) y(3:3:end) z(3:3:end)]');
+        
+        spec = [repmat(['%0.', precision, 'f '], 1, 9), '\n'];
+        
+        output = [x(1:3:end-2), y(1:3:end-2), z(1:3:end-2),...
+                  x(2:3:end-1), y(2:3:end-1), z(2:3:end-1),...
+                  x(3:3:end), y(3:3:end), z(3:3:end)]';
+              
+        fprintf(fid, spec, output);
+        
         switch upper(varargin{1})
             case 'LINES'
                 if mod(n_elements,2)==0
@@ -162,11 +246,11 @@ switch upper(dataType)
             case 'TRIANGLE'
                 ntri = length(varargin{5});
                 fprintf(fid,'\nPOLYGONS %d %d\n',ntri,4*ntri);
-                fprintf(fid,'3 %d %d %d\n',(varargin{5}-ones(ntri,3))');
+                fprintf(fid,'3 %d %d %d\n',(varargin{5}-1)');
             case 'TETRAHEDRON'
                 ntetra = length(varargin{5});
                 fprintf(fid,'\nPOLYGONS %d %d\n',ntetra,5*ntetra);
-                fprintf(fid,'4 %d %d %d %d\n',(varargin{5}-ones(ntetra,4))');
+                fprintf(fid,'4 %d %d %d %d\n',(varargin{5}-1)');
         end     
 end
 fclose(fid);
@@ -183,3 +267,13 @@ if strcmpi(filename,'matlab_export.vtk')
     end
 end
 end
+
+function setdataformat(fid, binaryflag)
+
+if ~binaryflag
+    fprintf(fid, 'ASCII\n');
+else
+    fprintf(fid, 'BINARY\n');
+end
+end
+    
