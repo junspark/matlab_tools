3bc3a37e43861d9004bfd3efb04c5d81502dcbb5
junspark <jun.sang.park@outlook.com>
2026-04-06 18:57:40 -0500

    DIC: add experiment config parser and clean filename construction
    This commit improves the DIC workflow by making experiment setup data explicit and easier to reuse, while also cleaning up frame filename generation.
    
    Changes included:
    
    - Refactor frame filename assembly in DIC_disp_strain.m to use sprintf with an explicit '<root>_<padded-number>.<ext>' format.
    
    - Add dic_read_exp_setup.m to parse key:value setup parameters from a markdown/text experiment info file.
    
    - Add dic_exp_info.md as a template/example setup file containing image series and ROI/control-point parameters.
    
    - Rename Documentation.docx to dic_documentation.docx for clearer naming consistency in DIC-tool.
    
    Notes:
    
    - Commit intentionally excludes unrelated untracked file '.loglogin'.

---
+++ DIC-tool/dic_exp_info.md
@@ -0,0 +1,34 @@
+# DIC Experiment Info
+
+## Image Series
+
+# Path to directory containing DIC images
+pname: /net/s1dserv/export/s1-idb/park_jul2013/DIC4Jun/DIC
+
+# File root name (prefix before the frame number)
+froot: DIC_
+
+# File extension
+fext: tif
+
+# Number of digits used in the frame number (e.g. 5 -> 00045)
+ndigits: 5
+
+# First, last, and increment of frame numbers
+fini: 45
+ffin: 421
+finc: 4
+
+## ROI & Control Points
+
+# Spacing between control points in horizontal and vertical directions (pixels)
+delta_h: 7
+delta_v: 7
+
+# Center of the ROI in horizontal and vertical directions (pixels)
+Hctr: 1436
+Vctr: 820
+
+# Half-width of ROI in horizontal and vertical directions (number of control point spacings)
+ws_h: 10
+ws_v: 20
---
+++ DIC-tool/dic_read_exp_setup.m
@@ -0,0 +1,82 @@
+function [pname, froot, fext, ndigits, fini, ffin, finc, ...
+    delta_h, delta_v, Hctr, Vctr, ws_h, ws_v] = dic_read_exp_setup(exp_info_file)
+% dic_read_exp_setup - reads DIC experiment setup values from markdown.
+%
+%   USAGE:
+%
+%   [pname, froot, fext, ndigits, fini, ffin, finc, ...
+%       delta_h, delta_v, Hctr, Vctr, ws_h, ws_v] = dic_read_exp_setup(exp_info_file)
+%
+%   INPUT:
+%
+%   exp_info_file
+%       Full path to dic_exp_info.md (or similar text file) containing
+%       key:value entries.
+%
+%   OUTPUT:
+%
+%   pname       Path to directory containing DIC images.
+%   froot       File root name (prefix before the frame number).
+%   fext        File extension (e.g. tif).
+%   ndigits     Number of digits used in the frame number.
+%   fini        First frame number.
+%   ffin        Last frame number.
+%   finc        Frame number increment.
+%   delta_h     Horizontal spacing between control points (pixels).
+%   delta_v     Vertical spacing between control points (pixels).
+%   Hctr        Horizontal center of ROI (pixels).
+%   Vctr        Vertical center of ROI (pixels).
+%   ws_h        Half-width of ROI in horizontal direction (control points).
+%   ws_v        Half-width of ROI in vertical direction (control points).
+%
+%   REQUIRED FILE FORMAT:
+%
+%   pname: <value>
+%   froot: <value>
+%   fext: <value>
+%   ndigits: <value>
+%   fini: <value>
+%   ffin: <value>
+%   finc: <value>
+%   delta_h: <value>
+%   delta_v: <value>
+%   Hctr: <value>
+%   Vctr: <value>
+%   ws_h: <value>
+%   ws_v: <value>
+
+if ~isfile(exp_info_file)
+    error('Missing setup file: %s', exp_info_file);
+end
+
+txt = fileread(exp_info_file);
+
+% Helper to parse a string value
+    function val = str_val(key)
+        tok = regexp(txt, sprintf('(?m)^\\s*%s\\s*:\\s*([^\\n]+)\\s*$', key), 'tokens', 'once');
+        if isempty(tok)
+            error('Key "%s" not found in %s', key, exp_info_file);
+        end
+        val = strtrim(tok{1});
+    end
+
+% Helper to parse a numeric value
+    function val = num_val(key)
+        val = str2double(str_val(key));
+    end
+
+pname   = str_val('pname');
+froot   = str_val('froot');
+fext    = str_val('fext');
+ndigits = num_val('ndigits');
+fini    = num_val('fini');
+ffin    = num_val('ffin');
+finc    = num_val('finc');
+delta_h = num_val('delta_h');
+delta_v = num_val('delta_v');
+Hctr    = num_val('Hctr');
+Vctr    = num_val('Vctr');
+ws_h    = num_val('ws_h');
+ws_v    = num_val('ws_v');
+
+end
